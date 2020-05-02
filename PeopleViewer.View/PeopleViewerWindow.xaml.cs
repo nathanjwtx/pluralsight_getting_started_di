72f265484f6ed765a6c40174fc815dfac2f1846a
Nathan <nathanjwtx@nathanjwtx.io>
2020-05-02 14:28:57 -0500

    added a view project. refactored peopleviewer to become the startup bootstrap. moved peopleviewwindow to view project

---
+++ PeopleViewer.View/PeopleViewerWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using PeopleViewer.Presentation;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class PeopleViewerWindow : Window
+    {
+        PeopleViewModel viewModel;
+
+        public PeopleViewerWindow(PeopleViewModel peopleViewModel)
+        {
+            // didn't create an interface for PeopleViewModel as the relationship is generally one-to-one which makes
+            // the intention clear
+            InitializeComponent();
+            viewModel = peopleViewModel;
+            this.DataContext = viewModel;
+        }
+
+        private void FetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.RefreshPeople();
+            ShowRepositoryType();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.ClearPeople();
+            ClearRepositoryType();
+        }
+
+        private void ShowRepositoryType()
+        {
+            RepositoryTypeTextBlock.Text = viewModel.DataReaderType;
+        }
+
+        private void ClearRepositoryType()
+        {
+            RepositoryTypeTextBlock.Text = string.Empty;
+        }
+    }
+}
